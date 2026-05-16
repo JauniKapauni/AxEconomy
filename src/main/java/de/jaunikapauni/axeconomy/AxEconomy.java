ae8ff15b1049c7937340687ab5c98f455fe18daa
JauniKapauni <riches-goods.54@icloud.com>
2026-05-16 22:44:12 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axeconomy/AxEconomy.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axeconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
