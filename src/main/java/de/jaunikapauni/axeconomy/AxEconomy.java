53d862d5cb4fd3aad897ce21ebdf81cdebf1201d
JauniKapauni <riches-goods.54@icloud.com>
2026-05-17 13:48:11 +0200

    feat: add DatabaseManager

--- src/main/java/de/jaunikapauni/axeconomy/AxEconomy.java
+++ src/main/java/de/jaunikapauni/axeconomy/AxEconomy.java
@@ -1,12 +1,28 @@
 package de.jaunikapauni.axeconomy;
 
+import de.jaunikapauni.axeconomy.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxEconomy extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        try{
+            databaseManager = new DatabaseManager(this);
+            if(databaseManager.initDatabaseTable1() == false){
+                getLogger().severe("Error creating balances table!");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
