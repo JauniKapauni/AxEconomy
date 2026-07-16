82fae6680e75bedbfe395b566e1715af8b46f55f
JauniKapauni <riches-goods.54@icloud.com>
2026-07-16 20:53:10 +0200

    fix(database): change db table structure

--- src/main/java/de/jaunikapauni/axeconomy/manager/DatabaseManager.java
+++ src/main/java/de/jaunikapauni/axeconomy/manager/DatabaseManager.java
@@ -37,7 +37,7 @@ public class DatabaseManager {
 
     public boolean initDatabaseTable1(){
         try(Connection conn = getConnection()){
-            try(PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS balances (uuid VARCHAR(255) NOT NULL PRIMARY KEY, balance DOUBLE)")){
+            try(PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS balances (uuid VARCHAR(36) NOT NULL PRIMARY KEY, balance DECIMAL(16, 2))")){
                 ps.executeUpdate();
                 return true;
             }
